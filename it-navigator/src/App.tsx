fd13d6c4351312ba14c06fd1f3f55b71671def2d
Michał Niedbalski <michnie507@student.polsl.pl>
2025-03-30 12:17:24 +0200

    Added template component certificates.

--- it-navigator/src/App.tsx
+++ it-navigator/src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "./components/theme-provider";
 import CreatingCV from "./components/creating-cv";
 import LinkedInProfile from "./components/linked-profile";
+import CertificatesOnResume from "./components/certificates-on-resume";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="flex flex-col items-center justify-center w-12/12 mx-auto">
         <CreatingCV />
         <LinkedInProfile />
+        <CertificatesOnResume/>
       </div>
     </ThemeProvider>
   );
--- it-navigator/src/components/creating-cv.tsx
+++ it-navigator/src/components/creating-cv.tsx
@@ -1,24 +1,31 @@
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
 
 function CreatingCV() {
   return (
-      <div className="flex flex-col items-center justify-center w-3/4 my-5">
-        <h1 className="text-3xl font-bold mb-6">How to create a resume?</h1>
-        <Card className="w-3/4">
-          <CardHeader>
-            <CardTitle>Step 1</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </CardContent>
-          <CardHeader>
-            <CardTitle>Step 2</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-          </CardContent>
-        </Card>
-      </div>
+    <div className="flex flex-col items-center justify-center w-3/4 my-5">
+      <Alert>
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      <h1 className="text-3xl font-bold mb-6">How to create a resume?</h1>
+      <Card className="w-3/4">
+        <CardHeader>
+          <CardTitle>Step 1</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </CardContent>
+        <CardHeader>
+          <CardTitle>Step 2</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
