11c967f1f8974e2e7ebf37f8e01a79acd3d157cb
Aleksandra Stuglik <al3xnra@gmail.com>
2025-05-02 22:06:25 +0200

    Add section about salaries in it

--- it-navigator/src/App.tsx
+++ it-navigator/src/App.tsx
@@ -5,6 +5,7 @@ import CertificatesOnResume from "./components/certificates-on-resume";
 import EducationInCV from "./components/education-in-cv";
 import UsefulCertificates from "./components/useful-certificates";
 import CVTemplateGuide from "./components/template-cv";
+import ITSalaryOverview from "./components/it-salary-overview";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <EducationInCV/>
         <CVTemplateGuide/>
         <UsefulCertificates/>
+        <ITSalaryOverview/>
       </div>
     </ThemeProvider>
   );
