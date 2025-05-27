301d960f5224e7a4119be419cf61a07172ada130
Artur <lancik25@gmail.com>
2025-05-27 21:04:50 +0200

    New component find-job-offers

--- it-navigator/src/App.tsx
+++ it-navigator/src/App.tsx
@@ -8,6 +8,7 @@ import UsefulCertificates from "./components/useful-certificates";
 import CVTemplateGuide from "./components/template-cv";
 import ITSalaryOverview from "./components/it-salary-overview";
 import HRquestions from "./components/HR-questions";
+import FindJobOffers from "./components/find-job-offers";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <UsefulCertificates/>
         <ITSalaryOverview/>
         <HRquestions/>
+        <FindJobOffers/>
       </div>
     </ThemeProvider>
   );
